d4107e57cf7242c3de0a1502a2833a94770d4ed2
verokim85 <vero.a.kim@gmail.com>
2018-05-26 11:51:27 -0400

    sinatra codealong completed

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :new
+  end
+
+  post '/student' do
+    @studnet = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses= Course.all
+
+    erb :student
+    
 end
